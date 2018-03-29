267af47e9cf7a7f0e2e43d84152dcb9c43eaf3ce
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-03-29 11:38:02 -0400

    Update and rename download_package.sh to download_ensembl.sh

---
+++ download_ensembl.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# Organization: MDIBL
+# Author: Lucie Hutchins
+# Date: September 2017
+# Modified: March 2018
+#
+# Wrapper script to download datasets from their download site. 
+# This creates an additional log that could be use later on
+#
+# This script is called by the download manager script.
+# Assumption: all the expected environment variables have been
+# sourced by the caller.
+#
+cd `dirname $0`
+WORKING_DIR=`pwd`
+SCRIPT_NAME=`basename $0`
+DATE=`date +"%B %d %Y"`
+DATE=`echo $DATE | sed -e 's/[[:space:]]/-/g'`
+
+WGET=`which wget`
+#
+# Set path of files on local server
+if [ "${GLOBAL_CONFIG}" = "" ]
+then
+    echo "ERROR: global environment GLOBAL_CONFIG not set " 
+    exit 1
+fi
+if [ "${PACKAGE_CONFIG_FILE}" = "" ]
+then
+    echo "ERROR: global environment PACKAGE_CONFIG_FILE not set " 
+    exit 1
+fi
+if [ "${PACKAGE_BASE}" = "" ]
+then
+    echo "ERROR: global environment PACKAGE_BASE not set " 
+    exit 1
+fi
+source ./${GLOBAL_CONFIG}
+source ./${PACKAGE_CONFIG_FILE}
+
+LOG=${DOWNLOADS_LOG_DIR}/${SCRIPT_NAME}.${SHORT_NAME}.${RELEASE_DIR}.log
+WGET_COMMAND="${WGET_OPTIONS} '${REMOTE_URL}'"
+rm -f ${LOG}
+touch ${LOG}
+
+if [ "${DOWNLOADS_LOG_DIR}" = "" ]
+then
+    echo "ERROR: global environment DOWNLOADS_LOG_DIR not set "  | tee -a ${LOG}   
+    exit 1
+fi
+if [ "${REMOTE_URL}" = "" ]
+then
+    echo "ERROR: global environment REMOTE_URL not set "  | tee -a ${LOG}   
+    exit 1
+fi
+echo "==" | tee -a ${LOG}  
+echo "Start Date:"`date` | tee -a ${LOG}  
+echo "Package: ${SHORT_NAME} - ${RELEASE_DIR}"  | tee -a ${LOG}  
+echo "Remote site: ${REMOTE_SITE}"  | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo "Local directory: ${PACKAGE_BASE}" | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+[ ! -d ${PACKAGE_BASE} ] && mkdir --parents ${PACKAGE_BASE}
+(
+set -f
+for taxonomy in ${TAXA}
+do
+   for dataset in ${DATASETS}
+   do
+       REMOTE_DIR=${FTP_ROOT}/$RELEASE_DIR/${DATASETS_TYPE[$dataset]}
+       REMOTE_FILE=$taxonomy}/${dataset}/${DATASETS[$dataset]}
+       README_FILE=$taxonomy}/${dataset}/README
+       echo "Processing: ${REMOTE_FILE}"
+       echo " ${REMOTE_DIR}"
+   done
+ done
+ )
+ 
+echo "End Date:"`date` | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo ""
+exit 0
