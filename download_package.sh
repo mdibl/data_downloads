0e623dbde662ad94a89ce093bc188c18647d7f13
LucyHut <lucie.hutchins@yahoo.com>
2018-03-29 10:31:40 -0400

    Refactoring

--- download_package.sh
+++ download_package.sh
@@ -1,197 +1,119 @@
-#!/bin/sh 
+#!/bin/sh
 
+# Organization: MDIBL
+# Author: Lucie Hutchins
+# Date: September 2017
 #
-# Program: download_package 
+# Wrapper script to download a given package from its download site. 
+# This creates an additional log that could be use later on
 #
-# Original Author: lnh
+# This script is called by the runPackageInstall.sh script
+# Assumption: all the expected environment variables have been
+# sourced by the caller.
 #
-# Purpose:
-#
-#	Download files from a remote server to our local download directory.
-#       Each package will have its configuration file
-#
-# Usage : ./download_package package.config
-# 
-# Assumption: 
-# A) In Configuration file:
-#    1) EXTERNAL_DATA_BASE path is set 
-#    2) DOWNLOADS_LOG_DIR directory is set
-#
-# B) In package-specific configuration file, the following global variables are set:
-#    1) REMOTE_SITE  - package host - fetched from Configuration.default 
-#    2) REMOTE_SITE_URL  - package site url (ftp://$REMOTE_SITE, http://$REMOTE_SITE, or https://$REMOTE_SITE)
-#    3) REMOTE_DIR   - path to package on host
-#    4) REMOTE_FILES - list of packages/files to fetch from host
-#
-#
-
-# What it does:
-# 1) source the Configuration file 
-# 2) source the Package specific Configuration file to access global environment variables
-# 3) setup path to files locally and remotely
-# 4) if local_directory path does not exist create it
-# 5) foreach file on the list, downloads a local copy
-# 6) Write a log report
-# 7) exit
-#
-#
-# Note: This is a modification of what I implemented for MGI(mirror_wget)
-# 
-# Original Date: 2/25/2016
-# Modification Date: 8/3/2017
-#
-WGET=`which wget`
-
-if [ $# -lt 1 ]
-then
-   echo "-----------------------------------------------"
-   echo ""
-   echo "Usage: ./download_package annotation_config_file"
-   echo ""
-   echo "------------------------------------------------"
-   exit 1
-fi
 cd `dirname $0`
 WORKING_DIR=`pwd`
-THIS_CONFIG_FILE=$1
 SCRIPT_NAME=`basename $0`
-#SERVER_NAME=`hostname -a`
-SERVER_NAME=`uname -n`
-#
-# Check if package config file exists
-#
-if [ ! -f $THIS_CONFIG_FILE ]
-then
-  echo "The Configuration file $THIS_CONFIG_FILE is missing from $WORKING_DIR"
-  exit 1
-fi
-#
-#
-# Check if the main config file exists
-#
-MAIN_CONFIG=$WORKING_DIR/Configuration
+DATE=`date +"%B %d %Y"`
+DATE=`echo $DATE | sed -e 's/[[:space:]]/-/g'`
 
-if [ ! -r $MAIN_CONFIG ]
-then
-  echo "The main Configuration file is missing from $WORKING_DIR"
-  echo "Run the Install script "
-  exit 1
-fi
-# source the main config file
-. ${MAIN_CONFIG}
-
-#source the config file specific to these annotations
-. $THIS_CONFIG_FILE
-#
-#setup the log
-package_file=`basename $THIS_CONFIG_FILE`
-LOG_FILE="${DOWNLOADS_LOG_DIR}/$package_file.log"
-rm -rf $LOG_FILE
-touch $LOG_FILE
-date | tee -a $LOG_FILE
+WGET=`which wget`
 #
 # Set path of files on local server
-#
-if [ "$LOCAL_DIR" = "" ]
+#export GLOBAL_CONFIG PACKAGE_BASE  RELEASE_NUMBER PACKAGE_CONFIG_FILE
+if [ "${GLOBAL_CONFIG}" = "" ]
 then
-   LOCAL_DIR=$REMOTE_SITE$REMOTE_DIR
+    echo "ERROR: global environment GLOBAL_CONFIG not set " 
+    exit 1
 fi
-
-LOCAL_DOWNLOAD_BASE=${EXTERNAL_DATA_BASE}/$LOCAL_DIR
-
-#
-# This are instances where you waant to store
-if [ $loca_dir_is_absolute  ]
+if [ "${PACKAGE_CONFIG_FILE}" = "" ]
 then
-    LOCAL_DOWNLOAD_BASE=$LOCAL_DIR
+    echo "ERROR: global environment PACKAGE_CONFIG_FILE not set " 
+    exit 1
 fi
-#
-#LOG current setting 
-#
-echo "------------------------------" | tee -a $LOG_FILE
-echo "***** Remote site setting *****" | tee -a $LOG_FILE
-echo "------------------------------" | tee -a $LOG_FILE
-echo "Remote site: $REMOTE_SITE" | tee -a $LOG_FILE
-echo "Remote directory: $REMOTE_DIR" | tee -a $LOG_FILE
-echo "Files to download: $REMOTE_FILES" | tee -a $LOG_FILE
-echo "------------------------------" | tee -a $LOG_FILE
-echo "***** Local setting *****" | tee -a $LOG_FILE
-echo "------------------------------" | tee -a $LOG_FILE
-echo "Working directory is:$WORKING_DIR" | tee -a $LOG_FILE
-echo "Running $SCRIPT_NAME on Server :$SERVER_NAME" | tee -a $LOG_FILE
-echo "config file used: $THIS_CONFIG_FILE" | tee -a $LOG_FILE
-echo "download directory: $LOCAL_DOWNLOAD_BASE" | tee -a $LOG_FILE
-echo "download logs directory: $DOWNLOADS_LOG_DIR" | tee -a $LOG_FILE
-echo "log: $LOG_FILE" | tee -a $LOG_FILE
-echo "------------------------------" | tee -a $LOG_FILE
-
-#
-# Set path of files on local server
-# Make parent directories as needed
-if [ ! -d $LOCAL_DOWNLOAD_BASE ]
+if [ "${PACKAGE_BASE}" = "" ]
 then
-   echo "Creating new directory $LOCAL_DOWNLOAD_BASE"
-   mkdir -p $LOCAL_DOWNLOAD_BASE
+    echo "ERROR: global environment PACKAGE_BASE not set " 
+    exit 1
 fi
+source ./${GLOBAL_CONFIG}
+source ./${PACKAGE_CONFIG_FILE}
 
-# attempt to set write lock
-echo "Working directory is:$SERVER_NAME:`pwd`"
+LOG=${DOWNLOADS_LOG_DIR}/${SCRIPT_NAME}.${RELEASE_DIR}.log
+WGET_COMMAND="${WGET_OPTIONS} '${REMOTE_URL}'"
+rm -f ${LOG}
+touch ${LOG}
 
-if [ $is_xml_query ]
+if [ "${DOWNLOADS_LOG_DIR}" = "" ]
+then
+    echo "ERROR: global environment DOWNLOADS_LOG_DIR not set "  | tee -a ${LOG}   
+    exit 1
+fi
+if [ "${REMOTE_URL}" = "" ]
+then
+    echo "ERROR: global environment REMOTE_URL not set "  | tee -a ${LOG}   
+    exit 1
+fi
+echo "==" | tee -a ${LOG}  
+echo "Start Date:"`date` | tee -a ${LOG}  
+echo "Package: ${SHORT_NAME}"  | tee -a ${LOG}  
+echo "Remote site: ${REMOTE_SITE}"  | tee -a ${LOG}  
+echo "Remote directory: ${REMOTE_DIR}"  | tee -a ${LOG}  
+echo "Remote files: ${REMOTE_FILES}"  | tee -a ${LOG}  
+echo "Remote url: ${REMOTE_URL}" | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo "Local directory: ${PACKAGE_BASE}" | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo "Running $SCRIPT_NAME from: ${WORKING_DIR}"| tee -a ${LOG}  
+
+[ ! -d ${PACKAGE_BASE} ] && mkdir --parents ${PACKAGE_BASE}
+if [ ${is_xml_query} ]
 then
    ##don't do the loop if this is a xml query string
-   target_file="$REMOTE_SITE_URL$REMOTE_DIR/${REMOTE_FILES}"
-   echo "---- $WGET $WGET_OPTIONS $target_file" | tee -a  ${LOG_FILE}
-   cd ${LOCAL_DOWNLOAD_BASE}
-   $WGET -a ${LOG_FILE} $WGET_OPTIONS "$target_file"
+   target_file="${REMOTE_URL}/${REMOTE_FILES}"
+   echo "---- ${WGET} ${WGET_OPTIONS} $target_file" | tee -a  ${LOG}  
+   cd ${PACKAGE_BASE}
+   ${WGET} -a ${LOG} ${WGET_OPTIONS} "${target_file}"
 else
 (
 set -f
 for remote_file in ${REMOTE_FILES}
 do
-   echo "$remote_file"
-   target_file="$REMOTE_SITE_URL$REMOTE_DIR/$remote_file"
+   target_file="${REMOTE_URL}/${remote_file}"
+   echo "Downloading ${target_file}"
    ## get the directory part from the remote_file name
-   target_local_dir=`dirname $remote_file`
-   echo "Creating new directory $target_local_dir"
+   target_local_dir=`dirname ${remote_file}`
    #
    # Make parent directories as needed
-   if [ "$target_local_dir" != "" ]
+   if [ "${target_local_dir}" != "" ]
    then
-      if [ ! -d ${LOCAL_DOWNLOAD_BASE}/$target_local_dir ]
-      then
-          echo "Creating new directory ${LOCAL_DOWNLOAD_BASE}/$target_local_dir"
-          mkdir -p ${LOCAL_DOWNLOAD_BASE}/$target_local_dir
-      fi
+       if [ "${NO_LOCAL_PARENT_DIR}" = true ]
+       then 
+           target_local_dir=""
+       else
+           echo "Creating ${PACKAGE_BASE}/${target_local_dir}"
+           mkdir -p ${PACKAGE_BASE}/${target_local_dir}
+       fi
    fi
    #cd to LOCAL_DOWNLOAD_BASE and run wget command 
-   cd ${LOCAL_DOWNLOAD_BASE}/$target_local_dir 
+   cd ${PACKAGE_BASE}/${target_local_dir} 
    # delete local files as needed
-   if [ "$do_deletes" = true ]
+   if [ "${do_deletes}" = true ]
    then
-       rm -f $remote_file
+       rm -f ${remote_file}
    fi
-   if [ "$IS_HTTP_PATTERN" = true ]
+   if [ "${IS_HTTP_PATTERN}" = true ]
    then
-       $WGET -a ${LOG_FILE} $WGET_OPTIONS -A "$remote_file" "$REMOTE_SITE_URL$REMOTE_DIR/" 
+       ${WGET} -a ${LOG} ${WGET_OPTIONS} -A "${remote_file}" "${REMOTE_URL}/" 
     else
-       $WGET -a ${LOG_FILE} $WGET_OPTIONS "$target_file" 
+      echo "Running command: ${WGET} -a ${LOG} ${WGET_OPTIONS} ${target_file} "
+       ${WGET}  ${WGET_OPTIONS} ${target_file} 2>&1 | tee -a ${LOG}
     fi
-done
-)
-fi
-
-#
-# Create symbolic link for this source if needed
-#
-if [ "$create_src_symlink" = true ]
-then
-  cd $EXTERNAL_DATA_BASE
-  rm -f $source_name
-  ln -s $REMOTE_SITE $source_name
-fi
-
-echo "$REMOTE_SITE annotations downloaded" | tee -a  ${LOG_FILE}
-echo "Program complete" | tee -a  ${LOG_FILE}
-echo "================================="| tee -a  ${LOG_FILE}
+ done
+ )
+ fi
+ 
+echo "End Date:"`date` | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo ""
 exit 0
