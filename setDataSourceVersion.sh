6178e0103139c3e20cb826b306a27042f4f05933
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-03-29 10:02:01 -0400

    Update setDataSourceVersion.sh

--- setDataSourceVersion.sh
+++ setDataSourceVersion.sh
@@ -4,9 +4,8 @@
 # Author: Lucie Hutchins
 # Date: March 2018
 # What it does:
-#  1) downloads the current release Readme file (*.txt, *.html, ...) 
-#       from the downloads site - parses it to extract the latest release number
-#  2) sets the current release number flag
+#  1) sets the current release number flag with
+#.    a specific release version
 #
 #
 cd `dirname $0`
@@ -35,15 +34,17 @@ echo "
 "
 
 ## Validate usage
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-    echo "Usage: ./${SCRIPT_NAME} source_name"
-    echo "example: ./${SCRIPT_NAME} ensembl"
+    echo "Usage: ./${SCRIPT_NAME} source_name version"
+    echo "example: ./${SCRIPT_NAME} ensembl 91"
     displayDataSources
     exit 1
 fi
 
 SOURCE_NAME=$1
+RELEASE_NUMBER=$2
+
 if [ ! -d ${SOURCE_NAME} ]
 then
    echo "ERROR: No automation found for ${SOURCE_NAME}"
@@ -74,21 +75,6 @@ PACKAGE_DOWNLOADS_BASE=${EXTERNAL_DATA_BASE}/${SOURCE_NAME}
 RELEASE_FILE=${PACKAGE_DOWNLOADS_BASE}/${CURRENT_FLAG_FILE}
 [ ! -d ${PACKAGE_DOWNLOADS_BASE} ] && mkdir -p ${PACKAGE_DOWNLOADS_BASE}
 
-## Check if wget is installed 
-if [ ! -f ${WGET} ]
-then
-  echo "Can't use 'wget' command to download some of our packages "
-  echo "Wget not installed on `uname -n`"
-  exit 1 	
-fi
-## Check if git is installed 
-if [ ! -f ${GIT} ]
-then
-  echo "Can't use 'git' command to download some of our packages"
-  echo "Git not installed on `uname -n`"
-  exit 1 	
-fi
-
 LOG_FILE="${DOWNLOADS_LOG_DIR}/${SCRIPT_NAME}.${SOURCE_NAME}.log"
 
 rm -rf ${LOG_FILE}
@@ -96,43 +82,25 @@ touch ${LOG_FILE}
 
 ## set release number to current value in ${RELEASE_FILE}
 [ -f ${RELEASE_FILE} ] &&  RELEASE_NUMBER=`cat ${RELEASE_FILE}`
-echo "Checking ${SOURCE_NAME}'s  Current Release"| tee -a ${LOG_FILE}
-echo "The current version info is stored in  ${RELEASE_FILE}"| tee -a ${LOG_FILE}
-
-FILE_TOKEN=`basename ${REMOTE_VERSION_FILE}`
-LOCAL_VERSION_FILE=${PACKAGE_DOWNLOADS_BASE}/${FILE_TOKEN}
-echo "-------------------------------------"
-echo "Using Wget to Download the release version file"| tee -a ${LOG_FILE}
-echo "File to download:${REMOTE_VERSION_FILE}"| tee -a ${LOG_FILE}
-echo ""
-echo ">>>>>>>> Wget output starts here "
-${WGET}  -O ${LOCAL_VERSION_FILE} ${REMOTE_VERSION_FILE} 2>&1 | tee -a ${LOG_FILE}
-echo "<<<<<<<< Wget output ends here "
-echo ""
-if [ ! -f ${LOCAL_VERSION_FILE} ]
-then
-        echo "Download failed: ${LOCAL_VERSION_FILE} is missing" 
-        exit 1
-fi
-EXPRESSION="$EXP_PREFIX ${LOCAL_VERSION_FILE}"
-RELEASE_NUMBER=`${EXPRESSION} | grep "${VERSION_PREFIX}" |sed "s/${VERSION_PREFIX}//" `
-[ "${VERSION_SUFFIX}" != "" ] && RELEASE_NUMBER=`echo ${RELEASE_NUMBER} | sed "s/${VERSION_SUFFIX}//"`
 
 echo "------------------------------"
 RELEASE_NUMBER=`echo $RELEASE_NUMBER | sed -e 's/[[:space:]]*$//' | sed -e 's/^[[:space:]]*//'`
 
-## Create the current release Number file
-echo "${SOURCE_NAME} release: ${RELEASE_NUMBER}  detected"
+## Create/Update the current release Number file
+echo "Setting ${SOURCE_NAME} release number to : ${RELEASE_NUMBER}"
 if [[ ${RELEASE_NUMBER} =~ ${REPOS_TAG_PATTERN} ]]
 then
    rm -f ${RELEASE_FILE}
    touch ${RELEASE_FILE}
    echo "${RELEASE_NUMBER}" > ${RELEASE_FILE}
 fi
-if [ -f ${RELEASE_FILE} ]
+if [ ! -f ${RELEASE_FILE} ]
 then
-   RELEASE_NUMBER=`cat ${RELEASE_FILE}`
+   echo "Missing release flag file: ${RELEASE_FILE}"
+   exit 1
 fi
+RELEASE_NUMBER=`cat ${RELEASE_FILE}`
+echo "${SOURCE_NAME}'s Release version $RELEASE_NUMBER set in ${RELEASE_FILE}"| tee -a ${LOG_FILE}
 cd ${WORKING_DIR}
 source ./${PACKAGE_CONFIG_FILE}
 echo ""
