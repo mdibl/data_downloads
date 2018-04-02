6df14070ce5ee1cf2a231f3f00c544393b689a32
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-04-02 10:03:03 -0400

    Update runDownloadDataset.sh

--- runDownloadDataset.sh
+++ runDownloadDataset.sh
@@ -1,71 +1,85 @@
 #!/bin/sh
+#!/bin/sh
 
 # Organization: MDIBL
 # Author: Lucie Hutchins
-# Date: August 2017
+# Date: September 2017
+# Modified: April 2018
 #
-#Wrapper script to call the download script 
-# It creates an additional log that could be use later on
+# Wrapper script to call scripts that download  external data.
+# It creates an additional log that could be use later on.
 #
+# What it does:
+# 1) sources global configs
+# 2) Calls the appropriate download script to download the specified version of this datasets
+# 3) Updates the tool symbolic link on success 
+
 cd `dirname $0`
 WORKING_DIR=`pwd`
 SCRIPT_NAME=`basename $0`
+GLOBAL_CONFIG=Configuration
+
+function displayTools() {
+    echo ""
+    echo " List of available tools"
+    echo "----------------------------"
+    tools="`ls`"
+    for tool in ${tools}
+    do
+       [ -d ${tool} ] && echo " ${tool}"
+    done
+    echo ""
+}
+
+echo "
+***************************************
+ BIOCORE External Data Download AUTOMATION 
+***************************************
+A package to create automations that downloads
+commonly used bioinformatics datasets. 
+"
+
 
 if [ $# -lt 1 ]
 then
-  echo "Usage: ./$SCRIPT_NAME path2annotation_config"
-  echo "Example: ./$SCRIPT_NAME ensembl/ftp.ensembl.org.cdna.cfg"
+  echo "Usage: ./${SCRIPT_NAME} source_name"
+  echo "Example: ./${SCRIPT_NAME} ensembl"
+  displayTools
   exit 1
 fi
-#
-#Relative to current working directory
-#
-DOWNLOAD_SCRIPT=download_package
-ANNOT_CONFIG=$1
-MAIN_CONFIG=Configuration
-
-if [ ! -f $MAIN_CONFIG ]
+SOURCE_NAME=$1
+if [ ! -d ${SOURCE_NAME} ]
 then
-  echo "$MAIN_CONFIG file missing "     
-  exit 1
+   echo "ERROR: No automation found for ${SOURCE_NAME}"
+   echo "Check the spelling or the case sensitive"
+   displayTools
+   exit 1
 fi
-if [ ! -f $ANNOT_CONFIG ]
+##The config file is relative to
+# the root directory of package download 
+
+if [ ! -f ${GLOBAL_CONFIG} ]
 then
-  echo "'$ANNOT_CONFIG' file missing "     
+  echo "'${GLOBAL_CONFIG}' file missing under `pwd`" 
+  echo "You must run the setup.sh script first to generate this file"
+  echo "Usage: ./setup.sh "
   exit 1
 fi
-
-# get global environment variable from config files
+source ./${GLOBAL_CONFIG}
+# Create base directories if not exist
+# Make sure the directory is not empty or
+# the main root directory "/" 
 #
-source ./$MAIN_CONFIG
-source ./$ANNOT_CONFIG
+[ ! -d ${DOWNLOADS_LOG_DIR} ] && mkdir -p ${DOWNLOADS_LOG_DIR}
 
-LOG=$DOWNLOADS_LOG_DIR/$SCRIPT_NAME.$SHORT_NAME.$RELEASE_NUMBER.$DATASET.log
+LOG=$DOWNLOADS_LOG_DIR/$SCRIPT_NAME.${SOURCE_NAME}.log
 rm -f $LOG
 touch $LOG
 echo "==" | tee -a $LOG
 echo "Start Date:"`date` | tee -a $LOG
-echo "$SHORT_NAME Release: $RELEASE_NUMBER"  | tee -a $LOG
-echo "$SHORT_NAME Dataset: $DATASET"  | tee -a $LOG
-echo "Remote site: $REMOTE_SITE"  | tee -a $LOG
-echo "Remote directory: $REMOTE_DIR"  | tee -a $LOG
-echo "Local directory: $EXTERNAL_DATA_BASE/$LOCAL_DIR" | tee -a $LOG 
-echo "==" | tee -a $LOG
-echo "Remote files:" | tee -a $LOG
-echo " $REMOTE_FILES" | tee -a $LOG
-echo "==" | tee -a $LOG
-echo "Running script from: $WORKING_DIR"| tee -a $LOG
-echo "Command: ./$DOWNLOAD_SCRIPT $ANNOT_CONFIG"| tee -a $LOG
-echo "==" | tee -a $LOG
 
-./$DOWNLOAD_SCRIPT $ANNOT_CONFIG   2>&1 | tee -a $LOG
+./${SOURCE_NAME}/download.sh  2>&1 | tee -a $LOG
 
-echo "=="
-if [ $? -ne 0 ]
-then
-  echo "Status: FAILED" | tee -a $LOG
-  exit 1
-fi
 echo "Status: SUCCESS" | tee -a $LOG
 echo "=="
 echo "End Date:"`date` | tee -a $LOG
