7ee3a7f1445f55bc717fdb6030cfe632d2fee91e
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-04-04 13:06:52 -0400

    Update unzipFasta.sh

--- wormbase/unzipFasta.sh
+++ wormbase/unzipFasta.sh
@@ -18,6 +18,7 @@
 cd `dirname $0`
 SCRIPT_NAME=`basename $0`
 WORKING_DIR=`pwd`
+GUNZIP=`which  gunzip`
 
 PACKAGE_CONFIG=`basename ${PACKAGE_CONFIG_FILE}`
 if [ ! -f ${PACKAGE_CONFIG} ]
@@ -34,50 +35,40 @@ RELEASE_NUMBER=`cat ${RELEASE_FILE} | sed -e 's/[[:space:]]*$//' | sed -e 's/^[[
 
 source ./${PACKAGE_CONFIG}
 
-export PACKAGE_CONFIG_FILE PACKAGE_DOWNLOADS_BASE RELEASE_FILE DOWNLOADS_LOG_DIR MAIN_DOWNLOAD_SCRIPT
-SCRATCH_DIR="$SCRATCH_DATA_BASE/$SHORT_NAME-$RELEASE_NUMBER"
+SCRATCH_DIR=${SCRATCH_DATA_BASE}/${SHORT_NAME}-${RELEASE_NUMBER}
+PACKAGE_BASE=${PACKAGE_DOWNLOADS_BASE}/${ORGANISMS_DOWNLOAD_DIR}
 
-PACKAGE_BASE=${PACKAGE_DOWNLOADS_BASE}/${RELEASE_DIR}/species"
-if [ ! -d $SCRATCH_DIR ]
-then
-    mkdir -p $SCRATCH_DIR
-fi
-cd $SCRATCH_DIR
-for organism in $TAXA
+[ ! -d ${SCRATCH_DIR} ] && mkdir -p ${SCRATCH_DIR}
+cd ${SCRATCH_DIR}
+for organism in ${TAXA}
 do
-  organism_dir=$PACKAGE_BASE/$organism
+  organism_dir=${PACKAGE_BASE}/${organism}
   if [ ! -d $organism_dir ]
   then
      echo "ERROR: Missing directory $organism_dir"
      exit 1
   fi
-  echo "===== $organism ====="
-  for project in $PROJECTS
+  for dataset in "${!DATASETS[@]}"
   do
-    
-      for dataset in $FASTA_ANNOTATIONS
-      do
-        dataset_dir=$SCRATCH_DIR/$project-$dataset
-        FASTA_FILES=`ls $organism_dir | grep $project | grep $dataset |grep gz`
-        ## Next if this project does not include this dataset
-        [ -z "${FASTA_FILES}"  ] && continue 
-        mkdir -p $dataset_dir
-        cd $dataset_dir
-        echo "Unzipping $organism_dir/$project*.$dataset dataset under $dataset_dir" 
-        [ ! -d temp ] && mkdir temp
-        OLD_FASTAS=`ls | grep .fa`
-        [ -n "${OLD_FASTAS}" ] && mv *.fa temp
-        for fasta_file in $FASTA_FILES
-        do
-           if [ -f $organism_dir/$fasta_file ]
-           then
-               echo "$fasta_file" 
-               cp -p $organism_dir/$fasta_file .
-               gunzip $fasta_file
-           fi
-        done
-        rm -rf temp 
-      done
+     dataset_dir=${SCRATCH_DIR}/${organism}-${dataset}
+     FASTA_FILES=`ls ${organism_dir} | grep ${dataset}${ZIP_EXTENSION}`
+     ## Next if this project does not include this dataset
+     [ -z "${FASTA_FILES}"  ] && continue 
+     mkdir -p ${dataset_dir}
+     cd ${dataset_dir}
+     echo "Unzipping ${organism_dir/${dataset}${ZIP_EXTENSION} dataset under `pwd`" 
+     [ ! -d temp ] && mkdir temp
+     OLD_FASTAS=`ls | grep .fa`
+     [ -n "${OLD_FASTAS}" ] && mv *.fa temp
+     for fasta_file in ${FASTA_FILES}
+     do
+        if [ -f $organism_dir/$fasta_file ]
+        then
+              cp -p ${organism_dir}/${fasta_file} .
+              ${GUNZIP} ${fasta_file}
+        fi
+     done
+     rm -rf temp 
      ## Create the joined transcriptome 
      #
      if [ -d $SCRATCH_DIR/$project-mRNA_transcripts ]
