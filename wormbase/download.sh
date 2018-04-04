e6ed62127b78df382473bb238966c1085f179d21
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-04-04 11:32:36 -0400

    Update download.sh

--- wormbase/download.sh
+++ wormbase/download.sh
@@ -55,24 +55,19 @@ echo "==" | tee -a ${LOG}
 set -f
 for taxonomy in ${TAXA}
 do
-   for dataset in "${!DATASETS[@]}"
-   do
-       DOWNLOAD_DIR=${PACKAGE_BASE}/${SPECIES_DIR}/${taxonomy}
-       mkdir -p ${DOWNLOAD_DIR}
-       cd ${DOWNLOAD_DIR}
-       REMOTE_FILE=${SPECIES_DIR}/$taxonomy/${DATASETS[$dataset]}
-       ## Remote path to this dataset files
-       REMOTE_URL=${REMOTE_SITE}${REMOTE_DIR}/${REMOTE_FILE}${ZIP_EXTENSION}
-       remote_file=`basename ${REMOTE_URL}`
-       if [ "${IS_HTTP_PATTERN}" = true ]
-       then
+     DOWNLOAD_DIR=${PACKAGE_BASE}/${SPECIES_DIR}/${taxonomy}
+     mkdir -p ${DOWNLOAD_DIR}
+     cd ${DOWNLOAD_DIR}
+     REMOTE_FILE=${SPECIES_DIR}/$taxonomy/*
+     ## Remote path to this dataset files
+     REMOTE_URL=${REMOTE_SITE}${REMOTE_DIR}/${REMOTE_FILE}${ZIP_EXTENSION}
+     remote_file=`basename ${REMOTE_URL}`
+     if [ "${IS_HTTP_PATTERN}" = true ]
+     then
             ${WGET} ${WGET_OPTIONS} -A ${remote_file} "${REMOTE_URL}/" 
-       else
+     else
             ${WGET}  ${WGET_OPTIONS} ${REMOTE_URL} 
-       fi 
-      
-       
-   done 
+     fi 
 done
 )
 if [ "${ONTOLOGY}" != "" ]
