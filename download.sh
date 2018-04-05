6ac29ca4433e8e638bbccd7e9a01ac7c907665e7
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-04-05 15:03:29 -0400

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# Organization: MDIBL
+# Author: Lucie Hutchins
+# Date: April 2018
+#
+# Wrapper script to download datasets from their download site. 
+# This creates an additional log that could be use later on
+#
+# This script is called by the download manager script.
+# Assumption: all the expected environment variables have been
+# sourced by the caller.
+#
+cd `dirname $0`
+SCRIPT_NAME=`basename $0`
+DATE=`date +"%B %d %Y"`
+DATE=`echo $DATE | sed -e 's/[[:space:]]/-/g'`
+WGET=`which wget`
+
+PACKAGE_CONFIG=`basename ${PACKAGE_CONFIG_FILE}`
+if [ ! -f ${PACKAGE_CONFIG_FILE} ]
+then
+    echo "ERROR: global environment ${PACKAGE_CONFIG_FILE} missing from `pwd` " 
+    exit 1
+fi
+source ./${PACKAGE_CONFIG_FILE}
+
+PACKAGE_BASE=${PACKAGE_DOWNLOADS_BASE}
+
+LOG=${DOWNLOADS_LOG_DIR}/${SCRIPT_NAME}.${SHORT_NAME}.log
+rm -f ${LOG}
+touch ${LOG}
+if [ "${DOWNLOADS_LOG_DIR}" = "" ]
+then
+    echo "ERROR: global environment DOWNLOADS_LOG_DIR not set "  | tee -a ${LOG}   
+    exit 1
+fi
+echo "==" | tee -a ${LOG}  
+echo "Start Date:"`date` | tee -a ${LOG}  
+echo "Package: ${SHORT_NAME} - ${RELEASE_DIR}"  | tee -a ${LOG}  
+echo "Remote site: ${REMOTE_SITE}"  | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo "Local directory: ${PACKAGE_BASE}" | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+[ ! -d ${PACKAGE_BASE} ] && mkdir --parents ${PACKAGE_BASE}
+
+(
+set -f
+for dataset in "${!DATASETS[@]}"
+do
+  DOWNLOAD_DIR=${PACKAGE_BASE}/${dataset}
+  mkdir -p ${DOWNLOAD_DIR}
+  cd ${DOWNLOAD_DIR}
+  REMOTE_FILE=${DATASETS[$dataset]}
+  ## Remote path to this dataset files
+  REMOTE_URL=${REMOTE_SITE}$/${REMOTE_FILE}
+  remote_file=`basename ${REMOTE_URL}`
+  if [ "${IS_HTTP_PATTERN}" = true ]
+  then
+      ${WGET} ${WGET_OPTIONS} -A ${remote_file} "${REMOTE_URL}/" 
+  else
+      ${WGET}  ${WGET_OPTIONS} ${REMOTE_URL} 
+  fi 
+done
+)
+echo "End Date:`date`" | tee -a ${LOG}  
+echo "==" | tee -a ${LOG}  
+echo ""
+exit 0
