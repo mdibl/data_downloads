f61774954bedebda0827a685ba7d1f4cc836de6d
Lucie Hutchins <LucyHut@users.noreply.github.com>
2018-04-09 17:46:03 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -22,7 +22,7 @@ then
     echo "ERROR: global environment PACKAGE_CONFIG_FILE not set " 
     exit 1
 fi
-RELEASE_NUMBER=""
+
 if [ -f ${RELEASE_FILE} ]
 then 
    RELEASE_NUMBER=`cat ${RELEASE_FILE}`
